1eeaea7540d2a0dedddff2b1409b19d11e429ed2
ASKK <102436728+ASKiere@users.noreply.github.com>
2022-07-26 13:48:57 -0400

    efwefwef
    sdfsefswef

---
+++ main.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 6942
+
+app.get('/', (req, res) => {
+    let m = 100
+    let o = 1
+    let j = Math.random(o, m)
+    res.send(`Random number was ${j}!`)
+    if (j === 1) {
+        res.send('WOW! You got 1! Thats incredibly rare!')
+    }
+    if (j === 69) {
+        res.send('hahahaha funny')
+    }
+  })
+  
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
+  })
